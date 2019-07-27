cd72f691928c52f6b38155c410237037831af217
AlicanCopur <37244649+AlicanCopur@users.noreply.github.com>
2019-07-27 17:27:09 +0300

    Update WorldUpdater.php

--- src/AlicanCopur/WorldUpdater.php
+++ src/AlicanCopur/WorldUpdater.php
@@ -1,5 +1,21 @@
 <?php
 
+/** 
+*     _    _ _                  ____ 
+*    / \  | (_) ___ __ _ _ __  / ___|
+*   / _ \ | | |/ __/ _` | '_ \| |    
+*  / ___ \| | | (_| (_| | | | | |___ 
+* /_/   \_\_|_|\___\__,_|_| |_|\____|
+*                                 
+*                                  
+*  -I'm getting stronger if I'm not dying-
+*
+* @version 1.0
+* @author AlicanCopur
+* @copyright HashCube Network © | 2015 - 2019
+* @license Açık yazılım lisansı altındadır. Tüm hakları saklıdır. 
+*/                                   
+
 namespace AlicanCopur;
 
 use pocketmine\{
